4013a297319e73f28776933287b7c0237b1fb2b0
Trish Griego <trish.griego@moz.com>
2017-03-13 18:28:59 -0700

    slowly moving functionality of app built in codepen to live app

--- src/App.js
+++ src/App.js
@@ -9,10 +9,24 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+
+
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Another paragaraph</p>
+      <img src="https://www.placecage.com/gif/200/300" alt="Nick Cage"/>
+        <table>
+           <thead>
+             <tr>
+               <th>Name</th>
+               <th>Price</th>
+             </tr>
+           </thead>
+           <tbody>jj</tbody>
+         
+         </table>
       </div>
     );
   }
