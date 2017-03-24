ae2cc105aaa769a8f42ed36155b95d2c0f300e14
Trish Griego <trish.griego@moz.com>
2017-03-24 13:51:07 -0700

    Added dragability to a sinle table component. Haven't been able to isolate dragability.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// ES6
+import Draggable, {DraggableCore} from 'react-draggable'; // The default and <DraggableCore>
+
 
 class App extends Component {
+  eventLogger = (e: MouseEvent, data: Object) => {
+    console.log('Event ', event);
+    console.log('Data', data);
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,6 +21,44 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div>
+          <Draggable
+            axis="y"
+            handle=".handle"
+            defaultPosition={{x: 0, y: 0}}
+            position={null}
+            grid={[25,25]}
+            zIndex={100}
+            onStart={this.handleStart}
+            onDrag={this.handleDrag}
+            onStop={this.handleStop}>
+            <div>
+              <div className="handle">Drag from here</div>
+              <div>This Readme is dragging on ...</div>
+            <div>
+              <table>
+                <tr>
+                  <th>Engine</th>
+                  <th>Locale Regex</th>
+                  <th>Strategy</th>
+                  <th>Max Tries</th>
+                  <th>Proxy Balancers</th>
+                </tr>
+                <tr>
+                  <td>engine</td>
+                  <td>locale_regex</td>
+                  <td>strategy</td>
+                  <td>max_tries</td>
+                  <td>proxy_balancer</td>
+                </tr>
+              </table>
+            </div>
+            </div>
+          </Draggable>
+        </div>
+
+
       </div>
     );
   }
