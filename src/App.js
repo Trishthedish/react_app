c810a0c132665f5e4d20e31c56d046a95a760130
Trish Griego <trish.griego@moz.com>
2017-03-24 13:23:12 -0700

    added collections for use of virtualized tables.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Collection } from 'react-virtualized';
+import 'react-virtualized/styles.css'
 
 class App extends Component {
   render() {
@@ -9,24 +11,35 @@ class App extends Component {
 
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <h2>Welcome to Swappable</h2>
         </div>
 
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Another paragaraph</p>
-      <img src="https://www.placecage.com/gif/200/300" alt="Nick Cage"/>
-        <table>
-           <thead>
-             <tr>
-               <th>Name</th>
-               <th>Price</th>
-             </tr>
-           </thead>
-           <tbody>jj</tbody>
 
-         </table>
+    <table className="ReactVirtualized_Table">
+        <img src="https://www.placecage.com/gif/200/300" alt="Nick Cage"/>
+
+          <tbody>
+            <tr className="ReactVirtualized_Table_headerColumn">
+              <th className="ReactVirtualized_Table_headerColumn">Engine</th>
+              <th className="ReactVirtualized_Table_headerColumn">Locale Regex</th>
+              <th className="ReactVirtualized_Table_headerColumn">Locale Regex</th>
+              <th className="ReactVirtualized_Table_headerColumn">Strategy</th>
+              <th className="ReactVirtualized_Table_headerColumn">Max Tries</th>
+              <th >Proxy Balancers</th>
+            </tr>
+            <tr>
+              <td>google</td>
+              <td>ko-KR</td>
+              <td>first_page</td>
+              <td>0</td>
+              <td>google_ec2_us</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
